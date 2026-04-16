c1298352c7f7854386c5b8e67d206c3891619b67
stefanods29 <szamora2909@gmail.com>
2026-04-15 23:04:36 -0500

    Primer commit: archivos base del proyecto, esta vacio, este es el comienzo del proyecto

---
+++ src/main/java/Grupo4/ProyectoDesarrollo/Web/Integrado/AvanceProyectoApplication.java
@@ -0,0 +1,13 @@
+package Grupo4.ProyectoDesarrollo.Web.Integrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanceProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanceProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Grupo4/ProyectoDesarrollo/Web/Integrado/AvanceProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo4.ProyectoDesarrollo.Web.Integrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanceProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
