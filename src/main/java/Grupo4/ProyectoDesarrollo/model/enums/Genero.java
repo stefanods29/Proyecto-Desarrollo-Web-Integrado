93ace3936e45dbe6c84595137edf0cb7871ff1e8
stefanods29 <szamora2909@gmail.com>
2026-04-20 20:16:08 -0500

    Entidades Cita y Paciente creados, junto a los enums Genero, GrupoSanguineo y TipoDocumento

---
+++ src/main/java/Grupo4/ProyectoDesarrollo/model/enums/Genero.java
@@ -0,0 +1,7 @@
+package Grupo4.ProyectoDesarrollo.model.enums;
+
+public enum Genero {
+    MASCULINO,
+    FEMENINO,
+    OTRO
+}
---
+++ src/main/java/Grupo4/ProyectoDesarrollo/model/enums/GrupoSanguineo.java
@@ -0,0 +1,12 @@
+package Grupo4.ProyectoDesarrollo.model.enums;
+
+public enum GrupoSanguineo {
+    A_POSITIVO,
+    A_NEGATIVO,
+    B_POSITIVO,
+    B_NEGATIVO,
+    AB_POSITIVO,
+    AB_NEGATIVO,
+    O_POSITIVO,
+    O_NEGATIVO
+}
