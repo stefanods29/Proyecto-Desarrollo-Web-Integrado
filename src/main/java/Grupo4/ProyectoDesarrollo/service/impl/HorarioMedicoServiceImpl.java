14b8fbd2139874c209205ca5cedafc8a61f7585d
stefanods29 <szamora2909@gmail.com>
2026-04-22 22:13:12 -0500

    Implementa HorarioMedicoServiceImpl

--- src/main/java/Grupo4/ProyectoDesarrollo/service/impl/HorarioMedicoServiceImpl.java
+++ src/main/java/Grupo4/ProyectoDesarrollo/service/impl/HorarioMedicoServiceImpl.java
@@ -1,5 +1,36 @@
 package Grupo4.ProyectoDesarrollo.service.impl;
 
-public class HorarioMedicoServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+import Grupo4.ProyectoDesarrollo.model.HorarioMedico;
+import Grupo4.ProyectoDesarrollo.repository.HorarioMedicoRepository;
+import Grupo4.ProyectoDesarrollo.service.HorarioMedicoService;
+
+@Service
+@RequiredArgsConstructor
+public class HorarioMedicoServiceImpl implements HorarioMedicoService {
+
+    private final HorarioMedicoRepository horarioMedicoRepository;
+
+    @Override
+    public HorarioMedico crear(HorarioMedico horario) {
+        return horarioMedicoRepository.save(horario);
+    }
+
+    @Override
+    public List<HorarioMedico> listar() {
+        return horarioMedicoRepository.findAll();
+    }
+
+    @Override
+    public HorarioMedico buscarPorId(Long id) {
+        return horarioMedicoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        horarioMedicoRepository.deleteById(id);
+    }
 }
