d647da5d14f1d16ba195b151a20880ee805267e5
E Lujan Z <u23273066@utp.edu.pe>
2026-06-23 17:40:50 -0500

    Correción de archivo clínico

--- src/main/java/Grupo4/ProyectoDesarrollo/repository/ArchivoClinicoRepository.java
+++ src/main/java/Grupo4/ProyectoDesarrollo/repository/ArchivoClinicoRepository.java
@@ -1,12 +1,13 @@
 package Grupo4.ProyectoDesarrollo.repository;
 
-import Grupo4.ProyectoDesarrollo.model.ArchivoClinico;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import Grupo4.ProyectoDesarrollo.model.ArchivoClinico;
 
 @Repository
 public interface ArchivoClinicoRepository extends JpaRepository<ArchivoClinico, Long> {
@@ -14,4 +15,6 @@ public interface ArchivoClinicoRepository extends JpaRepository<ArchivoClinico, 
 
     @Query("SELECT a FROM ArchivoClinico a WHERE a.consultaMedica.paciente.id = :pacienteId")
     List<ArchivoClinico> buscarArchivosPorPacienteId(@Param("pacienteId") Long pacienteId);
+
+    List<ArchivoClinico> findByConsultaMedicaPacienteUsuarioUsername(String username);
 }
