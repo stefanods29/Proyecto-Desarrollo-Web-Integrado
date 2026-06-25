59e6ea52bbea7693c3d619a158c59bdebc8cd7fc
CRomeroB <u23219864@utp.edu.pe>
2026-06-25 03:09:48 -0500

    cambio en medicoDTO para que en la pagina de angular no salga el ID sino el nombre del medico.

--- src/main/java/Grupo4/ProyectoDesarrollo/dto/MedicoDTO.java
+++ src/main/java/Grupo4/ProyectoDesarrollo/dto/MedicoDTO.java
@@ -11,17 +11,32 @@ import lombok.*;
 public class MedicoDTO {
     private Long id;
     private String numeroColegiatura;
+
     private Long usuarioId;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String telefono;
+
     private Long especialidadId;
     private Long clinicaId;
     private Boolean activo;
 
     public static MedicoDTO fromEntity(Medico medico) {
         if (medico == null) return null;
+
+        Usuario usuario = medico.getUsuario();
+
         return MedicoDTO.builder()
                 .id(medico.getId())
                 .numeroColegiatura(medico.getNumeroColegiatura())
-                .usuarioId(medico.getUsuario() != null ? medico.getUsuario().getId() : null)
+
+                .usuarioId(usuario != null ? usuario.getId() : null)
+                .nombre(usuario != null ? usuario.getNombre() : null)
+                .apellido(usuario != null ? usuario.getApellido() : null)
+                .correo(usuario != null ? usuario.getCorreo() : null)
+                .telefono(usuario != null ? usuario.getTelefono() : null)
+
                 .especialidadId(medico.getEspecialidad() != null ? medico.getEspecialidad().getId() : null)
                 .clinicaId(medico.getClinica() != null ? medico.getClinica().getId() : null)
                 .activo(medico.getActivo())
