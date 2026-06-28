2f64604d8e0914c036ac07084617cfbdbb5659aa
E Lujan Z <u23273066@utp.edu.pe>
2026-06-27 22:08:53 -0500

    Vercel

--- src/main/java/Grupo4/ProyectoDesarrollo/config/SecurityConfig.java
+++ src/main/java/Grupo4/ProyectoDesarrollo/config/SecurityConfig.java
@@ -161,7 +161,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                Arrays.asList("http://localhost:4200", "https://frontend-desarrollo-web-integrado-g.vercel.app"));
+                Arrays.asList("http://localhost:4200","https://nova-salud-g.vercel.app" , "https://frontend-desarrollo-web-integrado-g.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT","PATCH" , "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
